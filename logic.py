129f19c3532fac7edffc75df56ec7ba0a4bb8d37
Tucker Carney <tucker.carney1@gmail.com>
2022-09-22 21:12:29 -0600

    initial raw move logic for paw, bishop, king

--- logic.py
+++ logic.py
@@ -5,3 +5,76 @@ class ChessLogic:
 
     def get_valid_moves(board: Board) -> List:
         moves = []
+
+    def _pawn_moves(rank: int, file: int) -> List:
+        
+        moves = []
+        moves.append((rank + 1, file))
+        moves.append((rank + 2, file))
+        moves.append((rank + 1, file + 1))
+        moves.append((rank + 1, file -1))
+        
+        return moves
+
+    def _king_moves(rank: int, file: int) -> List:
+        moves = []
+
+        moves.append((rank + 1, file))
+        moves.append((rank - 1, file))
+        moves.append((rank, file + 1))
+        moves.append((rank, file - 1))
+        moves.append((rank + 1, file + 1))
+        moves.append((rank + 1, file - 1))
+        moves.append((rank - 1, file + 1))
+        moves.append((rank - 1, file - 1))
+
+        return moves
+
+    def _bishop_moves(rank: int, file: int) -> List:
+        moves = []
+
+        cur_rank = rank
+        cur_file = file
+        cur_rank +=1
+        cur_file +=1
+        while ChessLogic._on_board(cur_rank, cur_file):
+            moves.append((cur_rank, cur_file))
+            cur_rank += 1
+            cur_file += 1
+        
+        cur_rank = rank
+        cur_file = file
+        cur_rank +=1
+        cur_file -=1
+        while ChessLogic._on_board(cur_rank, cur_file):
+            moves.append((cur_rank, cur_file))
+            cur_rank += 1
+            cur_file -= 1
+
+        cur_rank = rank
+        cur_file = file
+        cur_rank -=1
+        cur_file -=1
+        while ChessLogic._on_board(cur_rank, cur_file):
+            moves.append((cur_rank, cur_file))
+            cur_rank -= 1
+            cur_file -= 1
+        
+        cur_rank = rank
+        cur_file = file
+        cur_rank -=1
+        cur_file +=1
+        while ChessLogic._on_board(cur_rank, cur_file):
+            moves.append((cur_rank, cur_file))
+            cur_rank -= 1
+            cur_file += 1
+
+        return moves
+            
+    @staticmethod
+    def _on_board(rank: int, file: int) -> bool:
+        if rank < 0 or rank > 7:
+            return False
+        if file < 0 or file > 7:
+            return False
+        return True
